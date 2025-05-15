6a95b50fdd0dc4a6d93c983c536fe2552c6b32e3
Joel Erulu <joelerulu088@gmail.com>
2025-05-15 14:24:22 -0400

    Finished Password Generator

---
+++ index.js
@@ -0,0 +1,20 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordOutput1 = document.getElementById("passwordOutput1")
+let passwordOutput2 = document.getElementById("passwordOutput2")
+
+
+function generateRandomPassword(length) {
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  return password;
+}
+
+function passwordGenerator() {
+  passwordOutput1.textContent = generateRandomPassword(10); 
+  passwordOutput2.textContent = generateRandomPassword(10);
+}
